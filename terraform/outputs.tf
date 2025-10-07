184ebd7f8202a0d8014ef6a43c39c702c32a1982
Kalyani-Bambal <kalyanibambal97@gmail.com>
2025-10-07 18:36:21 +0000

    all chnages are updated

--- terraform/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "found_roles" {
-  value = [for r in data.aws_iam_role.required : r.name]
-}
--- terraform/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "region" {
-  description = "AWS region to run prechecks in"
-  type        = string
-}
-
-variable "role_names" {
-  description = "List of IAM role names to validate existence"
-  type        = list(string)
-}
